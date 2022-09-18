ad5b5fdd09b01ddb6c2a22d439fc7ea567c8ba31
Blazeyusuf <87114618+blazeyusuf@users.noreply.github.com>
2022-09-18 23:57:46 +0100

    Add files via upload

--- public/js/main.js
+++ public/js/main.js
@@ -1,71 +1,24 @@
-$(document).ready(function () {
-  window._token = $('meta[name="csrf-token"]').attr('content')
+$(document).ready(function() {
 
-  moment.updateLocale('en', {
-    week: {dow: 1} // Monday is the first day of the week
-  })
+    
+    var readURL = function(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
 
-  $('.date').datetimepicker({
-    format: 'YYYY-MM-DD',
-    locale: 'en',
-    icons: {
-      up: 'fas fa-chevron-up',
-      down: 'fas fa-chevron-down',
-      previous: 'fas fa-chevron-left',
-      next: 'fas fa-chevron-right'
+            reader.onload = function (e) {
+                $('.profile-pic').attr('src', e.target.result);
+            }
+    
+            reader.readAsDataURL(input.files[0]);
+        }
     }
-  })
+    
 
-  $('.datetime').datetimepicker({
-    format: 'YYYY-MM-DD HH:mm:ss',
-    locale: 'en',
-    sideBySide: true,
-    icons: {
-      up: 'fas fa-chevron-up',
-      down: 'fas fa-chevron-down',
-      previous: 'fas fa-chevron-left',
-      next: 'fas fa-chevron-right'
-    }
-  })
-
-  $('.timepicker').datetimepicker({
-    format: 'HH:mm:ss',
-    icons: {
-      up: 'fas fa-chevron-up',
-      down: 'fas fa-chevron-down',
-      previous: 'fas fa-chevron-left',
-      next: 'fas fa-chevron-right'
-    }
-  })
-
-  $('.select-all').click(function () {
-    let $select2 = $(this).parent().siblings('.select2')
-    $select2.find('option').prop('selected', 'selected')
-    $select2.trigger('change')
-  })
-  $('.deselect-all').click(function () {
-    let $select2 = $(this).parent().siblings('.select2')
-    $select2.find('option').prop('selected', '')
-    $select2.trigger('change')
-  })
-
-  $('.select2').select2()
-
-  $('.treeview').each(function () {
-    var shouldExpand = false
-    $(this).find('li').each(function () {
-      if ($(this).hasClass('active')) {
-        shouldExpand = true
-      }
-    })
-    if (shouldExpand) {
-      $(this).addClass('active')
-    }
-  })
-
-$('button.sidebar-toggler').click(function () {
-    setTimeout(function() {
-      $($.fn.dataTable.tables(true)).DataTable().columns.adjust();
-    }, 275);
-  })
-})
+    $(".file-upload").on('change', function(){
+        readURL(this);
+    });
+    
+    $(".upload-button").on('click', function() {
+       $(".file-upload").click();
+    });
+});
