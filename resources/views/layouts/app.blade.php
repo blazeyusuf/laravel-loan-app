55b87a75b50ac3e320e7778c4f380a47801bede4
Blazeyusuf <87114618+blazeyusuf@users.noreply.github.com>
2022-09-20 13:49:26 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="description"
         content="Banks charge a lot for overseas transfers. We don't. Transfer money abroad easily and quickly with our low cost money transfers." />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <meta name="author" content="Anthony Joboy (+2349035547107)" />
+    <meta name="author" content="Blazeyusuf" />
     <meta property="og:locale" content="en_US" />
     <meta property="og:type" content="website" />
     <meta property="og:title" content="{{ config('app.name') }}" />
